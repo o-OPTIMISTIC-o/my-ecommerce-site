ff9c6f4a0e7b41e1c4169bb8a24401d44600f9ed
tanakorn-kanoup <thanakorn0963363144@gmail.com>
2025-05-19 15:44:30 +0700

    edit commit

--- backend/apiServer.js
+++ backend/apiServer.js
@@ -13,7 +13,7 @@ app.use('/api/subscribe',require('./routes/subscribe'));
 app.use('/api/login',require('./routes/login'));
 app.use('/api/register',require('./routes/register'));
 app.use('/api/occupation', require('./routes/occupation'));
-app.listen(PORT,()=>{
-    console.log('Server running at http://localhost:'+PORT);
+app.listen(PORT, () => {
+    console.log('Server running at http://localhost:' + PORT);
 
 })
--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -1,10 +1,99 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+
+const sqlite3 = require("sqlite3").verbose();
+
+const dbPath = path.join(__dirname, '..', 'data', 'contact.db')
+const db = new sqlite3.Database(dbPath);
+
+db.run(`CREATE TABLE IF NOT EXISTS contact(
+        id INTEGER PRIMARY KEY,
+        fname TEXT,
+        lname TEXT,
+        email TEXT,
+        subject TEXT,
+        message TEXT)`)
 
 router.post('/', (req, res) => {
     const {fname, lname, email, subject, message} = req.body;
+    var sql = `INSERT INTO contact (
+                fname, 
+                lname, 
+                email, 
+                subject, 
+                message) VALUES ("`+ fname +`", "`+ lname +`", "`+ email +`", "`+ message +`")`;
+    
+    db.run('INSERT INTO contact (fname, lname, email, subject, message) VALUES (?,?,?,?,?)', 
+        [fname, lname, email, subject, message]);
+
     console.log('Content form submited', {fname, lname, email, subject, message});
-    res.status(200).json({status : 'Message received'});
+    res.status(200).json({status : 'Contact saved in database!!!'});
+});
+
+router.get('/:action', (req, res) => {
+    const { action } = req.params;
+
+    switch (action) {
+        case 'all':
+            var sql = "SELECT * FROM contact ORDER BY id DESC";
+            db.all(sql, [], (err, rows) => {
+                if(err) {
+                    return res.status(500).json({error : 'Fail to fetch contacts from DB!!!'})
+                } else {
+                    return res.json(rows);
+                }
+            });
+            break;
+
+        case 'last':
+            var sql = "SELECT * FROM contact WHERE id = (SELECT max(id) FROM contact)";
+            db.all(sql, [], (err, rows) => {
+                if(err) {
+                    return res.status(500).json({error : 'Fail to fetch contacts from DB!!!'})
+                } else {
+                    return res.json(rows);
+                }
+            });
+            break;
+
+        case 'before_last':
+            var sql = "SELECT * FROM contact WHERE id = (SELECT max(id) FROM contact WHERE id < (SELECT max(id) FROM contact))";
+            db.all(sql, [], (err, rows) => {
+                if(err) {
+                    return res.status(500).json({error : 'Fail to fetch contacts from DB!!!'})
+                } else {
+                    return res.json(rows);
+                }
+            });
+            break;
+
+        case 'last2':
+            var sql = "SELECT * FROM contact ORDER BY id DESC LIMIT 2";
+            db.all(sql, [], (err, rows) => {
+                if(err) {
+                    return res.status(500).json({error : 'Fail to fetch contacts from DB!!!'})
+                } else {
+                    return res.json(rows);
+                }
+            });
+            break;
+
+        case 'position3':
+            var sql = "SELECT * FROM contact WHERE id = 3";
+            db.all(sql, [], (err, rows) => {
+                if(err) {
+                    return res.status(500).json({error : 'Fail to fetch contacts from DB!!!'})
+                } else {
+                    return res.json(rows);
+                }
+            });
+            break;
+
+        default:
+            res.status(400).json({error : 'Unknow action'})
+            break;
+    }
 });
 
 module.exports = router;
